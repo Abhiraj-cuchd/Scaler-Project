f5d7b818da2863c7838c9f63c4b2c41efc50fc4b
Abhiraj-cuchd <21bca1058@cuchd.in>
2023-04-02 23:01:28 +0530

    Beta Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import { Router , Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import Sidebar from './Components/Sidebar/Sidebar';
+import Home from './Pages/Home';
+import AddBooking from './Components/AddBooking';
+
+
+const App = () => {
+
+  
+
+  const [toggle, setToggle] = useState(true);
+  const Toggle = () => {
+    setToggle(!toggle);
+  }
+
+  return (
+    <div className='container-fluid bg-secondary min-vh-100'>
+      <div className="row">
+        { toggle && <div className="col-2 bg-white vh-100">
+          <Sidebar />
+        </div>}
+        
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<Home Toggle={Toggle} />} />
+            <Route path='/create' element={<AddBooking />} />
+          </Routes>
+        </Router>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
